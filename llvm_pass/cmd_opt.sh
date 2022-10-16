ab56a072b2dc29459320a1c654978877280477c5
Tong Gao <tongg@synopsys.com>
2022-10-16 09:17:01 +0800

    Remove llvm_pass/

--- llvm_pass/cmd_opt.sh
+++
@@ -1,5 +0,0 @@
-# opt -disable-output -passes=helloworld ./a.ll
-# opt -disable-output -passes=globalopt ./a.ll
-
-clang -c -emit-llvm hello.c -o hello.bc
-opt -load /slowfs/cn59sig01/usr/tongg/cit_compilers/llvm-project/mybuilddir/lib/LLVMHello.so -hello2 -enable-new-pm=0 < hello.bc > /dev/null
